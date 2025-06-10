df1ec72a6f4c7ad0ad34f131a2935f0a8ee1b6aa
Emmanuel Oudot <manuo1@free.fr>
2025-06-10 20:54:03 +0200

    feat(profile): first iteration of Profile page structure with user first name display (User Story #4 - Prénom)
    - Setup basic layout and styling for the Profile page
    - Fetch user main data by ID from URL params
    - Display user first name on dashboard header
    - Prepare placeholders for future components in the page layout

---
+++ sportsee/frontend/sportsee/src/pages/ProfilePage.jsx
@@ -0,0 +1,52 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import userService from "../mocks/userService";
+import styles from "./ProfilePage.module.scss";
+
+function ProfilePage() {
+  const { id: userId } = useParams();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    userService.getUserMainData(parseInt(userId)).then((data) => setUser(data));
+  }, [userId]);
+
+  if (!user) return <div>Loading...</div>;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h1 className={styles.title}>
+          Bonjour{" "}
+          <span className={styles.firstname}>{user.userInfos.firstName}</span>
+        </h1>
+        <p className={styles.encouragement}>
+          Félicitation ! Vous avez explosé vos objectifs hier 👏
+        </p>
+      </div>
+
+      <div className={styles.dashboard}>
+        <div className={styles.leftColumn}>
+          <div className={styles.leftColumn__top}>leftColumn__top</div>
+          <div className={styles.leftColumn__bottom}>
+            <div className={styles.leftColumn__bottom__left}>
+              leftColumn__bottom__left
+            </div>{" "}
+            <div className={styles.leftColumn__bottom__center}>
+              leftColumn__bottom__center
+            </div>{" "}
+            <div className={styles.leftColumn__bottom__right}>
+              leftColumn__bottom__right
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.rightColumn}>
+          <div className={styles.rightColumn__center}>rightColumn__center</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProfilePage;
--- sportsee/frontend/sportsee/src/router/Router.jsx
+++ sportsee/frontend/sportsee/src/router/Router.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import Layout from "../pages/common/Layout";
 import Error404 from "../pages/Error404";
+import ProfilePage from "../pages/ProfilePage";
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
+        <Route path="/profil/:id" element={<ProfilePage />} />
         <Route path="/404" element={<Error404 />} />
         <Route path="*" element={<Error404 />} />
       </Route>
