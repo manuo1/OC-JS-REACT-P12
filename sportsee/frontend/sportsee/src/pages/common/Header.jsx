36864568c20a836ecee1f5338f55297ff5b4d444
Emmanuel Oudot <manuo1@free.fr>
2025-06-11 09:01:43 +0200

    feat: add Header component with logo and navigation links

--- sportsee/frontend/sportsee/src/pages/common/Header.jsx
+++ sportsee/frontend/sportsee/src/pages/common/Header.jsx
@@ -1,7 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./Header.module.scss";
+import logo from "../../assets/sportsee_logo.png";
 
 const Header = () => {
-  return <div className={styles.container}>Temporary Header</div>;
+  return (
+    <header className={styles.container}>
+      <div className={styles.logo}>
+        <img src={logo} alt="Kasa logo" />
+      </div>
+      <nav className={styles.nav}>
+        <Link to="/" className={styles.link}>
+          Accueil
+        </Link>
+        <Link to="/profil" className={styles.link}>
+          Profil
+        </Link>
+        <Link to="/settings" className={styles.link}>
+          Réglages
+        </Link>
+        <Link to="/community" className={styles.link}>
+          Communauté
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
